44e6a1fdaa7379917871ff8d6216eff487a68837
sultan <153429280+sul-t@users.noreply.github.com>
2025-03-17 22:57:09 +0500

    cheking the health of the tests

--- auth/conftest.py
+++ auth/conftest.py
@@ -1,14 +1,42 @@
 import pytest
-from django.core.cache import cache
+from httpx import AsyncClient
+from beanie import init_beanie
+from src.core.model import User
+from motor.motor_asyncio import AsyncIOMotorClient
+from src.core.config import settings
+import asyncio
 
-# fmt: off
-pytest_plugins = [
-]
-# fmt: on
+
+# @pytest.fixture(scope="session")
+# def event_loop():
+#     try:
+#         loop = asyncio.get_running_loop()
+#     except RuntimeError:
+#         loop = asyncio.new_event_loop()
+#     yield loop
+#     loop.close()
+
+
+@pytest.fixture(scope="module")
+async def async_client():
+
+    async with AsyncClient(base_url="http://127.0.0.1:8000") as client:
+        yield client
 
 
 @pytest.fixture(autouse=True)
-def _cache():
-    """Clear django cache after each test run."""
-    yield
-    cache.clear()
+async def clearing_mongo():
+    client = AsyncIOMotorClient(
+        f"mongodb://{settings.DB_USER}:{settings.DB_PASSWORD}@localhost:{settings.DB_PORT}"
+    )
+
+    try:
+        await init_beanie(database=client["ticket"], document_models=[User])
+        await User.find({}).delete_many()
+
+        yield
+    except Exception as e:
+        print(f"Error: {e}")
+
+        raise
+
--- auth/src/core/config.py
+++ auth/src/core/config.py
@@ -14,11 +14,11 @@ class Setting(BaseSettings):
     model_config = SettingsConfigDict(env_file=Path(__file__).resolve().parents[2] / '.env')
 
 
-config = Setting()
+settings = Setting()
 
 
 def get_db_uri() -> str:
-    return f'mongodb://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}'
+    return f'mongodb://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}'
 
 def get_auth_data() -> dict[str, str]:
-    return {'secret_key': config.SECRET_KEY, 'algorithm': config.ALGORITHM}
+    return {'secret_key': settings.SECRET_KEY, 'algorithm': settings.ALGORITHM}
